5ed510e2fbb5388edfe750326edf405cbb837eb5
federicohermo <federicohermo@gmail.com>
2024-10-06 15:17:53 -0300

    indexcss

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'; // Import createRoot instead of ReactDOM
 import App from './App';
+import './styles/index.css'
 
 const container = document.getElementById('root') as HTMLElement; // Find the root element
 const root = createRoot(container); // Create a root
